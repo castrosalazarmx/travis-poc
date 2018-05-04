0e073bf91f064f0b76261b5fd7894fadccf72ad9
Ricardo Garcia <ricardo@wizeline.com>
2018-05-04 02:49:41 -0500

    fix scripts

--- packages/api/scripts/deploy.sh
+++ packages/api/scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp ../../keyfile-dev.json .
+gcloud config set project $2
 gcloud auth activate-service-account --key-file keyfile-dev.json
 gcloud app deploy app.yaml -q --version $1
