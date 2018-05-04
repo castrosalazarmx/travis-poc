8565e8b559e9814701cebed07aaa0b8446f04e41
Ricardo Garcia <ricardo@wizeline.com>
2018-05-04 15:32:54 -0500

    science

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp ../../keyfile-dev.json .
 gcloud config set project $2
-gcloud auth activate-service-account --key-file keyfile-dev.json
+gcloud auth activate-service-account --key-file ../keyfile-dev.json
 gcloud app deploy app.yaml -q --version $1
