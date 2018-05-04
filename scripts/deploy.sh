376900be2b64bc0e80e4354f8fcde240810503d2
Ricardo Garcia <ricardo@wizeline.com>
2018-05-04 02:35:40 -0500

    fix scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 openssl aes-256-cbc -K $encrypted_a21a9babead1_key -iv $encrypted_a21a9babead1_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
-ls
+tar -xzf credentials.tar.gz
 lerna run deploy $1
